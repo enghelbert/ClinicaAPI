cfe4c392afc6e2d60cb1ddf5b4e5447b3ea1fec6
Enghel <019100568J@uandina.edu.pe>
2026-05-13 14:37:25 -0500

    Ejercicio 01: Extensión modelo Paciente con nuevos campos

--- ClinicaAPI/Models/Paciente.cs
+++ ClinicaAPI/Models/Paciente.cs
@@ -6,5 +6,11 @@
         public string Nombre { get; set; }
         public int Edad { get; set; }
         public string Diagnostico { get; set; }
+
+        // Campos nuevos
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string CorreoElectronico { get; set; }
+        public DateTime FechaRegistro { get; set; }
     }
 }
